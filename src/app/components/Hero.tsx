41dafe084c87482b321a2e016f1d305330bada89
Moin Latif <72666676+Moin105@users.noreply.github.com>
2024-02-29 23:00:33 +0500

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -32,7 +32,7 @@ export default function Heroo() {
           </span>
         </p>
       </div>
-      <div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-center gap-4">
+      <div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-center gap-4 relative z-10" >
         <h1 className="relative z-10 text-2xl md:text-6xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
           Sheikh Construction & Renovation
         </h1>
